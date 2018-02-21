84356def769cab2bc272e52b4ab0cd47b31b0c64
Kevin Buffardi <kbuffardi@csuchico.edu>
2018-02-20 23:18:57 -0800

    Simplify menu

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+  string list[5]; //array of 5 strings
+  int numItems = 0;
+  char input;
+
+  cout<<"\n==GROCERY LIST MANAGER==";
+  cout<<"\nEnter your choice: ";
+  cout<<"\n (A)dd an item";
+  cout<<"\n (Q)uit";
+  cout<<"\nYour choice (A/Q): ";
+  cin>>input;
+
+  return 0;
+}
