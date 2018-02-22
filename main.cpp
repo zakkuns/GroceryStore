fdba4112c672384f3eb4937811207c7b6dfd7faa
zakkuns <zkuns@mail.csuchico.edu>
2018-02-22 11:45:19 -0800

    Prompts the user to enter in ony 5 items on their grocery list

--- main.cpp
+++ main.cpp
@@ -1,19 +1,46 @@
+//Author: Zakary Kuns
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string input2;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+        do {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
 
-  return 0;
+                if (input == 'A' || input == 'a') {
+
+                        cout << "What is the item?" << endl;
+                        cin >> input2;
+
+                        if (numItems < 5) {
+                                list[numItems];
+                                numItems++;
+                        }
+                        
+                        else {
+                        cout << "You'll need a bigger list!" <<endl;
+                        
+                        }
+                
+
+
+                }
+        }
+
+        while (input != 'Q' && input != 'q'); 
+
+
+
+        return 0;
 }
